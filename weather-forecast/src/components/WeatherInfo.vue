b0d2e384678d12cd747f8a8652b76ee7bc6a047d
Axel Calås <axel.calas.semst@gmail.com>
2024-02-28 17:26:28 +0100

    Added LocalStorage for history and error-handling.

--- weather-forecast/src/components/WeatherInfo.vue
+++ weather-forecast/src/components/WeatherInfo.vue
@@ -23,12 +23,12 @@ export default {
             apiKey: "0fb98056d14c0b3b443c610b4ebe30e9",
             apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
             city: null,
-            resultCity: "",
-            resultTemp: "",
-            resultWeather: "",
+            resultCity: null,
+            resultTemp: null,
+            resultWeather: null,
             latitude: null,    // För senare anrop av långtids-prognos
             longitude: null,   // För senare anrop av långtids-prognos
-            searchHistory: [],
+            searchHistory: JSON.parse(localStorage.getItem('searchHistory')) || [], //Hämtar data från LocalStorage, är LocalStorage tom, gör arrayen tom. 
         };
     },
     methods: {
@@ -36,26 +36,31 @@ export default {
             const url = `${this.apiUrl}?appid=${this.apiKey}&q=${this.city}&units=metric`
             const response = await fetch(url);
             const result = await response.json();
+            this.city = "";
+
+            console.log(result)
+
+            if(result.cod != "200"){ //Felhantering så att inte undefined blir inlagt i historik-array. 
+                alert(`Error: ${result.cod}, ${result.message} \nPlease try again!`);
+                return;
+            }
 
             this.resultCity = result.name;
             this.resultTemp = `${Math.round(result.main.temp)}°C`;
             this.resultWeather = result.weather[0].description;
-
             this.latitude = result.coord.lat;
             this.longitude = result.coord.lon;
 
-            this.addToHistory({
+            this.AddToHistory({
                 city: this.resultCity,
                 temperature: this.resultTemp,
                 weather: this.resultWeather,
             })
-
-            console.log(result)
         },
-        addToHistory(search) {
+        AddToHistory(search) {
             this.searchHistory.unshift(search); // Lägger till element i början av array
-
             this.searchHistory = this.searchHistory.slice(0, 3); // Begränsar antalet element i array till 3, som visas på skärmen. 
+            localStorage.setItem('searchHistory', JSON.stringify(this.searchHistory)); //Lägger till arrayen i LocalStorage.
         }
     }
 }
